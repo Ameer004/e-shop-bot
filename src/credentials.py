b4c68ded44df497f0b8d0b59f1b86ae9fb4fe3ac
Ameer004 <ameer1808m@gmail.com>
2021-09-30 15:59:13 +0100

    Update credentials.py

--- src/credentials.py
+++ src/credentials.py
@@ -1,8 +1,8 @@
 # your twilio auth tokens | keys
 ACC_SID       = '<twilio-acc-sid>'
 AUTH_TOKEN    = '<twilio-auth-token>'
-WHATSAPP_TO   = 'whatsapp:+2637xxxxxxx'            # your twilio signed number
-WHATSAPP_FROM = 'whatsapp:+141552xxxxx'
+WHATSAPP_TO   = 'whatsapp:+13126673227'            # your twilio signed number
+WHATSAPP_FROM = 'whatsapp:+14155238886'
 
 # Admin contact or Agent in charge of Bot contact, can be any international number as long you stick to the format
 ADMIN_CONTACT = "whatsapp:+2349073338893"
